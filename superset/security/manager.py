6bcdc907e71208ccebba1876beecb968ad90ebd0
“5x-rahul” <rahulchaudhari@Rahuls-MacBook-Air.local>
2024-04-09 11:45:58 +0530

    update superset roles to 5X roles

--- superset/security/manager.py
+++ superset/security/manager.py
@@ -821,10 +821,9 @@ class SupersetSecurityManager(  # pylint: disable=too-many-public-methods
         pvms = self._get_all_pvms()
 
         # Creating default roles
-        self.set_role("Admin", self._is_admin_pvm, pvms)
-        self.set_role("Alpha", self._is_alpha_pvm, pvms)
-        self.set_role("Gamma", self._is_gamma_pvm, pvms)
-        self.set_role("sql_lab", self._is_sql_lab_pvm, pvms)
+        self.set_role("Workspace Admin", self._is_admin_pvm, pvms)
+        self.set_role("Primary Contributor", self._is_alpha_pvm, pvms)
+        self.set_role("Limited Contributor", self._is_gamma_pvm, pvms)
 
         # Configure public role
         if current_app.config["PUBLIC_ROLE_LIKE"]:
