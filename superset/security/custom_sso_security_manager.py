8040877f910ecaa24400eee925ba0808a667c8f8
Shruti Seth <shruti@5x.co>
2024-04-19 17:47:15 +0530

    update in auth0 props and security manager

--- superset/security/custom_sso_security_manager.py
+++ superset/security/custom_sso_security_manager.py
@@ -1,18 +1,27 @@
+from flask import redirect, flash, request
 from superset.security import SupersetSecurityManager
-import logging
-
+from flask_appbuilder.security.views import AuthDBView
+from flask_appbuilder.security.views import expose
+from flask_login import login_user
+from flask_jwt_extended import decode_token
+class CustomAuthDBView(AuthDBView):
+    @expose('/login/', methods=['GET', 'POST'])
+    def login(self):
+        token = request.args.get('token')
+        sm = self.appbuilder.sm
+        session = sm.get_session
+        if not token:
+            return super(CustomAuthDBView,self).login()
+        try:
+            token = decode_token(token)
+            id = token["sub"]
+            user = session.query(sm.user_model).filter_by(id=id).first()
+            login_user(user, remember=False, force=True)
+            return redirect(self.appbuilder.get_url_for_index)
+        except:
+            flash('Unable to auto login', 'warning')
+            return super(CustomAuthDBView,self).login()
 class CustomSsoSecurityManager(SupersetSecurityManager):
-    def oauth_user_info(self, provider, response=None):
-        if provider == 'auth0':
-            res = self.appbuilder.sm.oauth_remotes[provider].get('5xdev.us.auth0.com/userinfo')
-            me = res.json()
-            logging.info(" user_data: %s", me)
-
-            prefix = 'Superset'
-            return {
-                'username' : me['email'],
-                'name' : me['name'],
-                'email' : me['email'],
-                'first_name': me['email'],
-                'last_name': me['name'],
-            }
+    authdbview = CustomAuthDBView
+    def __init__(self, appbuilder):
+        super(CustomSsoSecurityManager, self).__init__(appbuilder)
